301d82e1d14efe216e92cc02e14fe6569e1f4c8d
henkyaliazki <henkyaliazkipratama>
2022-10-28 13:19:03 +0700

    fixinghit scrolling jQuery

--- js/apps.js
+++ js/apps.js
@@ -7,9 +7,16 @@ $(document).ready(function () {
   $(window).scroll(function () {
     let scroll = $(window).scrollTop();
     if (scroll > 300) {
-      $("nav").addClass("bg-secondary");
+      $("nav").addClass("bg-white");
     } else {
-      $("nav").removeClass("bg-secondary");
+      $("nav").removeClass("bg-white");
     }
   });
+  // buat sticky tidakmenutupi heder
+  $(".pageScroll").on("click", function (e) {
+    let address = $(this).attr("href");
+    let addressTo = $(address);
+    $("html,body").animate({ scrollTop: addressTo.offset().top - 50 });
+  });
+  e.preventDefault();
 });
